c54710ed95ec394413559413b0892e11da0cf521
Chandler <chandler.dalton@customerdynamics.com>
2022-12-05 23:55:11 -0700

    API Server

--- README.md
+++ README.md
@@ -1,1 +1,102 @@
 # WebLanguagesII
+
+# APIs
+
+# TODOS
+
+GET /todos
+
+returns all toDos
+
+-
+
+POST /todo
+body example
+{
+    "todo":  {
+        "name": "Clean Room",
+        "done": false,
+        "id": '4564',
+        "categories": []
+    }
+}
+
+returns status and updated array
+
+-
+
+PUT /todo
+body example
+{
+    "todo":  {
+        "name": "Clean Room",
+        "done": false,
+        "id": '4564',
+        "categories": []
+    }
+}
+
+returns status and updated array
+
+-
+
+DELETE /todo
+body example
+{
+    "todo":  {
+        "name": "Clean Room",
+        "done": false,
+        "id": '4564',
+        "categories": []
+    }
+}
+
+returns status and updated array
+
+# Categories
+
+GET /categories
+
+returns all categories
+
+-
+
+POST /category
+body example
+{
+    "category":  {
+        "id": "4862",
+        "name": "chores",
+        "color": "#69fe43"
+    }
+}
+
+returns status and updated array
+
+-
+
+PUT /category
+body example
+{
+    "category":  {
+        "id": "4862",
+        "name": "chores",
+        "color": "#69fe43"
+    }
+}
+
+returns status and updated array
+
+-
+
+DELETE /category
+body example
+{
+    "category":  {
+        "id": "4862",
+        "name": "chores",
+        "color": "#69fe43"
+    }
+}
+
+returns status and updated array
---
+++ ToDoApp/client/index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const port = 6000
+
+const toDos = [];
+
+const categories = [];
+
+app.get('/todos', (req, res) => {
+    res.send(toDos)
+});
+
+app.post('/todo', (req, res) => {
+    if(req.body.todo){
+        toDos = [...toDos, req.body.todo];
+        res.status(200).send(toDos)
+    } else {
+        res.status(403).send({msg: 'Request Body must contain a todo'})
+    }
+});
+
+app.put('/todo', (req, res) => {
+    if(req.body.todo){
+        const id = req.body.todo.id;
+        const index = toDos.findIndex(item => item.id === id);
+        toDos[index] = req.body.todo
+        res.status(200).send(toDos)
+    } else {
+        res.status(403).send({msg: 'Request Body must contain a todo'})
+    }
+});
+
+app.delete('/todo', (req, res) => {
+    if(req.body.todo){
+        const id = req.body.todo.id;
+        const index = toDos.findIndex(item => item.id === id);
+        toDos.splice(index, 1);
+        res.status(200).send(toDos)
+    } else {
+        res.status(403).send({msg: 'Request Body must contain a todo'})
+    }
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+});
+
+app.post('/category', (req, res) => {
+    if(req.body.category){
+        categories = [...categories, req.body.category];
+        res.status(200).send(categories)
+    } else {
+        res.status(403).send({msg: 'Request Body must contain a category'})
+    }
+});
+
+app.put('/category', (req, res) => {
+    if(req.body.category){
+        const id = req.body.category.id;
+        const index = categories.findIndex(item => item.id === id);
+        categories[index] = req.body.category
+        res.status(200).send(categories)
+    } else {
+        res.status(403).send({msg: 'Request Body must contain a category'})
+    }
+});
+
+app.delete('/category', (req, res) => {
+    if(req.body.category){
+        const id = req.body.category.id;
+        const index = categories.findIndex(item => item.id === id);
+        categories.splice(index, 1);
+        res.status(200).send(categories)
+    } else {
+        res.status(403).send({msg: 'Request Body must contain a category'})
+    }
+});

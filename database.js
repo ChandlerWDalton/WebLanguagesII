46b61fc0773eadc3e7dd33138edc0249c492df23
Chandler <chandler.dalton@customerdynamics.com>
2022-12-12 22:56:31 -0700

    Mongoose package

--- database.js
+++ database.js
@@ -1,8 +1,9 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const uri = "mongodb+srv://todo_user:cQzs4y1PdAPDcaQn@cluster0.081q2sn.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+mongoose.connect(
+    uri,{
+        usedNewUrlParser: true,
+        usesrUnifiedToplogy: true
+    }
+)
